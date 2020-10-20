21c74f484eb45fa23c1eb5f1bfe98ad5c1a043c1
Christian Santamaria <christiancameselle@hotmail.com>
2020-10-20 10:18:26 +0200

    [ADD] website_sale_product_search_amin: Work in progress

---
+++ website_sale_product_search_main/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 ...
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Website Sale Product Search Main',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'Solvos',
+    'website': 'https://github.com/solvosci/slv-e-commerce',
+    'license': 'LGPL-3',
+    'summary': 'Moves product search box from product page to the main page',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/website_sale_template.xml'
+    ],
+    'installable': True,
+}
