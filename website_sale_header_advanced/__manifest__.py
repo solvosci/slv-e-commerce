4a226af6e2d02919d5c60ea11a25c723e07aaf08
Christian Santamaria <christiancameselle@hotmail.com>
2020-12-09 13:49:28 +0100

    [ADD] website_sale_header_advanced, website_sale_theme_socel: Work in progress

--- website_sale_header_advanced/__manifest__.py
+++ website_sale_header_advanced/__manifest__.py
@@ -1,18 +1,14 @@
 # Copyright 2020 ...
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Website Sale Product Search Main',
+    'name': 'Website Sale Header Advance',
     'version': '12.0.1.0.0',
     'category': 'Website',
     'author': 'Solvos',
     'website': 'https://github.com/solvosci/slv-e-commerce',
     'license': 'LGPL-3',
-    'summary': 'Moves product search box from product page to the main page',
-    'depends': [
-        'website_sale',
-    ],
-    'data': [
-        'views/website_sale_template.xml'
-    ],
+    'summary': 'Header vertical edit',
+    'depends': ['website_sale',],
+    'data': ['views/website_sale_template.xml'],
     'installable': True,
 }
---
+++ website_sale_theme_socel/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ...
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Website Style Theme Socel',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'Solvos',
+    'website': 'https://github.com/solvosci/slv-e-commerce',
+    'license': 'LGPL-3',
+    'summary': 'Styles Theme Socel',
+    'depends': ['website_sale',],
+    'data': ['views/website_sale_template.xml'],
+    'installable': True,
+}
