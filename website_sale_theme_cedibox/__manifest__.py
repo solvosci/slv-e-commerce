48f15e6253775ad31b2eb24a9857e219ac485b25
ChristianSantamaria <christiancameselle@hotmail.com>
2021-06-23 14:15:24 +0200

    [ADD] website_sale_theme_cedibox

---
+++ website_sale_theme_cedibox/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 ...
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Website Sale Theme Cedibox',
+    'version': '13.0.1.0.0',
+    'category': 'Website',
+    'author': 'Solvos',
+    'website': 'https://github.com/solvosci/slv-e-commerce',
+    'license': 'LGPL-3',
+    'summary': 'Styles Theme Cedibox',
+    'depends': ['website_sale'],
+    'data': [
+        'templates/templates.xml',
+        'views/website_settings_view.xml'
+        ],
+    'installable': True,
+}
