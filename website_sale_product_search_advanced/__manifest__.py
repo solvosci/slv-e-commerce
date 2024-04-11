90f33da0d368ad400a7885ab0486f2180d59e4e7
Christian Santamaria <christian.santamaria@solvos.es>
2024-04-11 08:47:54 +0200

    [ADD] website_sale_product_search_advanced

---
+++ website_sale_product_search_advanced/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+{
+    "name": "Website Sale Product Search Advanced",
+    "summary": """
+        Adds an improvement to the website search to be able to search by:
+        - Partner Ref
+        - Barcode
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "12.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/solvosci/slv-e-commerce",
+    "depends": ["website_sale"],
+    "data": [
+        'views/product_template_views.xml'
+    ],
+    'installable': True,
+}
