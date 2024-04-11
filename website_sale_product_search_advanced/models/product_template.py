3f867843f9e520b90db8129b2797fd4a5783ef8a
Christian Santamaria <christian.santamaria@solvos.es>
2024-04-11 08:47:54 +0200

    [ADD] website_sale_product_search_advanced

---
+++ website_sale_product_search_advanced/models/product_template.py
@@ -0,0 +1,12 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    website_partner_ref = fields.Char(
+        string='Partner Reference'
+    )
