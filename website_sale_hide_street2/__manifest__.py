c9c6012c71a4e88582c95fcb208c50ec194fbb95
Anxo82 <angel.rivas@solvos.es>
2024-04-19 09:14:13 +0200

    [ADD] website_sale_hide_street2

---
+++ website_sale_hide_street2/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Website Sale Hide Street2",
+    "summary": """
+        Hides field street2 from ecommerce checkout
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "E-Commerce",
+    "website": "https://github.com/solvosci/slv-e-commerce",
+    "depends": ["website_sale"],
+    "data": [
+        "views/website_sale_template.xml",
+    ],
+    'installable': True,
+}
