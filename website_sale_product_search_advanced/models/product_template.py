d535bff06d17daf2d00dbb86f7d7065811a48bbd
Christian Santamaria <christian.santamaria@solvos.es>
2024-07-08 09:06:09 +0200

    [MIG] website_sale_product_search_advanced: Migration to 17.0

--- website_sale_product_search_advanced/models/product_template.py
+++ website_sale_product_search_advanced/models/product_template.py
@@ -1,7 +1,7 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
@@ -10,3 +10,10 @@ class ProductTemplate(models.Model):
     website_partner_ref = fields.Char(
         string='Partner Reference'
     )
+
+    @api.model
+    def _search_get_detail(self, website, order, options):
+        res = super()._search_get_detail(website, order, options)
+        res['search_fields'].append('product_variant_ids.barcode')
+        res['search_fields'].append('website_partner_ref')
+        return res
