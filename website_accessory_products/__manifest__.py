198583773c575fbfbb5692c656b04ac1536d51c5
Ángel Rivas <angel.rivas@solvos.es>
2025-02-27 15:58:08 +0100

    [ADD] website_accessory_products

---
+++ website_accessory_products/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Website accessory products",
+    "summary": """
+        Add automatically display accessory products in website product"
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/solvosci/slv-e-commerce",
+    "depends": ['website_sale'],
+    "data": [
+        "views/website_sale_template.xml",
+    ],
+    "installable": True,
+}
